46e1c39fa8fa0f784e6730494c8912f9e40a4d8c
Melwyn Saldanha <melwyn.s@peopleinteractive.in>
2019-09-30 06:15:23 +0530

    Updates

---
+++ precache-manifest.8c7d25a5d857f823c5a030ca266e0986.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "520195ccaf090d8ea2d1693d513aede6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "760c136300c677b10eeb",
+    "url": "/static/css/main.2bc15185.chunk.css"
+  },
+  {
+    "revision": "b160b725b44a24d1fcda",
+    "url": "/static/js/2.d8ea9c8f.chunk.js"
+  },
+  {
+    "revision": "760c136300c677b10eeb",
+    "url": "/static/js/main.9bd40cde.chunk.js"
+  },
+  {
+    "revision": "5e7475a9db513cda2643",
+    "url": "/static/js/runtime-main.46dea5f0.js"
+  }
+]);
